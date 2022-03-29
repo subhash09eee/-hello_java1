42afa987e80d82be61bc81c1a72f9f4799f346c9
subbu <subhash09eee@gmail.com>
2022-03-29 16:17:07 +0000

    practicing webhooks

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,12 @@ public class HelloWorld {
    
     System.out.println("Hello tuesday");
    System.out.println("Hello tuesday");
-    System.out.println("sunday monday");  
+    System.out.println("sunday monday");
+   System.out.println("sunday monday");
+    System.out.println("bye bye bye bye bye");
+
+   System.out.println("sunday monday");
+
+   System.out.println("sunday monday");  
    }
 }
